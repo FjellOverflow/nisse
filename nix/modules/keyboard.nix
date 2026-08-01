3d7dbe4fd64beea4db5df9d70783ff705714912c
FjellOverflow <fjelloverflow@protonmail.com>
2026-08-01 16:20:02 +0200

    fix(nix): keyboard config

--- nix/modules/keyboard.nix
+++ nix/modules/keyboard.nix
@@ -1,4 +1,10 @@
-{ lib, variables, ... }:
+{
+  config,
+  lib,
+  user,
+  variables,
+  ...
+}:
 
 {
   services.xserver.xkb = {
@@ -6,4 +12,15 @@
     variant = lib.mkDefault "";
   };
   console.keyMap = lib.mkDefault variables.keyboardLayout;
+
+  home-manager.users.${user} = _: {
+    dconf.settings."org/gnome/desktop/input-sources" = {
+      sources = lib.mkDefault [
+        (lib.gvariant.mkTuple [
+          "xkb"
+          config.services.xserver.xkb.layout
+        ])
+      ];
+    };
+  };
 }
