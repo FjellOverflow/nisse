aea064de4f370f2ea93e1f00350f490f660355b3
FjellOverflow <fjelloverflow@protonmail.com>
2026-08-02 09:32:56 +0200

    fix: leave numeric unlocalized

--- nix/base/common.nix
+++ nix/base/common.nix
@@ -13,9 +13,8 @@
 
   time.timeZone = variables.timeZone;
   i18n.defaultLocale = variables.defaultLocale;
-  i18n.extraLocaleSettings = lib.genAttrs [
+    i18n.extraLocaleSettings = lib.genAttrs [
     "LC_TIME"
-    "LC_NUMERIC"
     "LC_MONETARY"
     "LC_PAPER"
     "LC_MEASUREMENT"
